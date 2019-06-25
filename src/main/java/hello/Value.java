efbcefcad0b236aab61ce062b1f55e9d5f57e77b
analuciabolico <analuciabolico.dev@gmail.com>
2019-06-25 16:48:42 -0300

    #02

--- src/main/java/hello/Value.java
+++ src/main/java/hello/Value.java
@@ -1,12 +1,13 @@
 package hello;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Value {
 
     private Long id;
-    private String quote;
+    private String phrase;
 
     public Value() {
     }
@@ -14,24 +15,24 @@ public class Value {
     public Long getId() {
         return this.id;
     }
-
-    public String getQuote() {
-        return this.quote;
+    @JsonProperty("quote")
+    public String getPhrase() {
+        return this.phrase;
     }
 
     public void setId(Long id) {
         this.id = id;
     }
-
-    public void setQuote(String quote) {
-        this.quote = quote;
+    @JsonProperty("quote")
+    public void setPhrase(String phrase) {
+        this.phrase = phrase;
     }
 
     @Override
     public String toString() {
         return "Value{" +
                 "id=" + id +
-                ", quote='" + quote + '\'' +
+                ", phrase='" + phrase + '\'' +
                 '}';
     }
 }
